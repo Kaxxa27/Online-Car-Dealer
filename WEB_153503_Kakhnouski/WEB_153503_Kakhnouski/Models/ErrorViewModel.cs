b363b2bc5936d457319c19c6b30162df914bfb01
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-09-20 11:30:49 +0300

    Project WEB_153503_Kakhnouski added.

---
+++ WEB_153503_Kakhnouski/WEB_153503_Kakhnouski/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WEB_153503_Kakhnouski.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
