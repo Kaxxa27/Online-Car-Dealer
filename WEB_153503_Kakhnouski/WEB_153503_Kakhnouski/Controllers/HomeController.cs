e6f2166e581c4e64d6041a6912d38c0573906080
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-09-20 12:13:39 +0300

    Added HomeController with view.

---
+++ WEB_153503_Kakhnouski/WEB_153503_Kakhnouski/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_153503_Kakhnouski.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
