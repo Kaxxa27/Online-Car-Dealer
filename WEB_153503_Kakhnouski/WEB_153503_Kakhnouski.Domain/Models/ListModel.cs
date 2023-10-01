1abe48c56bc76f8eeb0356137bf19e9219c6f6d6
Eugene_Kakhnouski <Kaxxa2927@mail.ru>
2023-10-01 13:13:14 +0300

    Added ListModel and ResposeData in Domain.

---
+++ WEB_153503_Kakhnouski/WEB_153503_Kakhnouski.Domain/Models/ListModel.cs
@@ -0,0 +1,13 @@
+﻿namespace WEB_153503_Kakhnouski.Domain.Models;
+
+public class ListModel<T>
+{
+    // Requested list of objects
+    public List<T> Items { get; set; } = new();
+
+    // Current page number
+    public int CurrentPage { get; set; } = 1;
+
+    // Total number of pages
+    public int TotalPages { get; set; } = 1;
+}
---
+++ WEB_153503_Kakhnouski/WEB_153503_Kakhnouski.Domain/Models/ResponseData.cs
@@ -0,0 +1,13 @@
+﻿namespace WEB_153503_Kakhnouski.Domain.Models;
+
+public class ResponseData<T>
+{
+    // Requested data
+    public T Data { get; set; }
+    
+    // Indication of successful completion of the request
+    public bool Success { get; set; }
+
+    // Message in case of unsuccessful completion
+    public string? ErrorMessage { get; set; }
+}
