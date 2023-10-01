021542de76cd1045e5382061aadbe2aa509e0e2b
Eugene_Kakhnouski <Kaxxa2927@mail.ru>
2023-10-01 13:12:48 +0300

    Added class library named WEB_153503_Kakhnouski.Domain. Added Car and Category entities.

---
+++ WEB_153503_Kakhnouski/WEB_153503_Kakhnouski.Domain/Entities/Car.cs
@@ -0,0 +1,11 @@
+﻿namespace WEB_153503_Kakhnouski.Domain.Entities;
+
+public class Car
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!
+    public string? Description { get; set; }
+    public double Price { get; set; }
+    public Category? CategoryId { get; set; }
+    public string? Image { get; set; }
+}
