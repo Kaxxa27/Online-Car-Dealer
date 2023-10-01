54042b8ac3326b75e8c6f216c22baa04dd31cf48
Eugene_Kakhnouski <Kaxxa2927@mail.ru>
2023-10-01 14:12:57 +0300

    Added CarCategoryService.

---
+++ WEB_153503_Kakhnouski/WEB_153503_Kakhnouski/Services/CarCategoryService/ICarCategoryService.cs
@@ -0,0 +1,13 @@
+﻿using WEB_153503_Kakhnouski.Domain.Entities;
+using WEB_153503_Kakhnouski.Domain.Models;
+
+namespace WEB_153503_Kakhnouski.Services.CategoryServicep;
+
+public interface ICarCategoryService
+{
+    /// <summary>
+    /// Getting a list of all categories
+    /// </summary>
+    /// <returns> ResponseData<List<Category>> </returns>
+    public Task<ResponseData<List<Category>>> GetCategoryListAsync();
+}
---
+++ WEB_153503_Kakhnouski/WEB_153503_Kakhnouski/Services/CarCategoryService/MemoryCarCategoryService.cs
@@ -0,0 +1,25 @@
+﻿using WEB_153503_Kakhnouski.Domain.Entities;
+using WEB_153503_Kakhnouski.Domain.Models;
+using WEB_153503_Kakhnouski.Services.CategoryServicep;
+
+namespace WEB_153503_Kakhnouski.Services.CarCategoryService;
+
+public class MemoryCarCategoryService : ICarCategoryService
+{
+    public async Task<ResponseData<List<Category>>> GetCategoryListAsync()
+    {
+        var categories = new List<Category>() 
+        { 
+            new Category {Id=1, Name="A", 
+                NormalizedName="motorcycles"},
+            new Category {Id=2, Name="B", 
+                NormalizedName="сars"},
+            new Category {Id=3, Name="C", 
+                NormalizedName="trucks"},
+            new Category {Id=4, Name="D", 
+                NormalizedName="buses"},
+        };
+
+        return new ResponseData<List<Category>>() { Data = categories };
+    }
+}
