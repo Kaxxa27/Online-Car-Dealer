d4266641f794ef4d32ca8f80f243aebe1e938cd4
Eugene_Kakhnouski <Kaxxa2927@mail.ru>
2023-10-18 16:58:22 +0300

    Added CreateDB migration.

--- WEB_153503_Kakhnouski/WEB_153503_Kakhnouski.API/Data/AppDbContext.cs
+++ WEB_153503_Kakhnouski/WEB_153503_Kakhnouski.API/Data/AppDbContext.cs
@@ -1,6 +1,12 @@
-﻿namespace WEB_153503_Kakhnouski.API.Data
+﻿using Microsoft.EntityFrameworkCore;
+using WEB_153503_Kakhnouski.Domain.Entities;
+
+namespace WEB_153503_Kakhnouski.API.Data;
+
+public class AppDbContext : DbContext
 {
-    public class AppDbContext
-    {
-    }
+    public DbSet<Car> Cars { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
 }
---
+++ WEB_153503_Kakhnouski/WEB_153503_Kakhnouski.API/Migrations/20231018135601_CreateDB.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WEB_153503_Kakhnouski.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    NormalizedName = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    Price = table.Column<double>(type: "REAL", nullable: false),
+                    CategoryId = table.Column<int>(type: "INTEGER", nullable: true),
+                    Image = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cars_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cars_CategoryId",
+                table: "Cars",
+                column: "CategoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cars");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
