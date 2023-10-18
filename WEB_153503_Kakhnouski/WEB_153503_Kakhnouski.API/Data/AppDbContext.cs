b91ec0e492c99d2b8b562a219169fd7761241246
Eugene_Kakhnouski <Kaxxa2927@mail.ru>
2023-10-18 16:54:46 +0300

    Added DbContext.

---
+++ WEB_153503_Kakhnouski/WEB_153503_Kakhnouski.API/Data/AppDbContext.cs
@@ -0,0 +1,6 @@
+﻿namespace WEB_153503_Kakhnouski.API.Data
+{
+    public class AppDbContext
+    {
+    }
+}
--- WEB_153503_Kakhnouski/WEB_153503_Kakhnouski.API/Program.cs
+++ WEB_153503_Kakhnouski/WEB_153503_Kakhnouski.API/Program.cs
@@ -1,37 +1,45 @@
 
-namespace WEB_153503_Kakhnouski.API
+using Microsoft.EntityFrameworkCore;
+using WEB_153503_Kakhnouski.API.Data;
+
+namespace WEB_153503_Kakhnouski.API;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+
+        // Database configuration.
+        var connection = builder.Configuration.GetConnectionString("Default");
+        builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(connection));
+
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseStaticFiles();
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+
+        app.UseStaticFiles();
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
