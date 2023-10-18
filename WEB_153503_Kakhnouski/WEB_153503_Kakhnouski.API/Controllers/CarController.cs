952fc3b20def25729db4d0bf5d720ed6c50adb91
Eugene_Kakhnouski <Kaxxa2927@mail.ru>
2023-10-18 17:39:20 +0300

    CarController added.

---
+++ WEB_153503_Kakhnouski/WEB_153503_Kakhnouski.API/Controllers/CarController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WEB_153503_Kakhnouski.API.Data;
+using WEB_153503_Kakhnouski.Domain.Entities;
+
+namespace WEB_153503_Kakhnouski.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CarController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Cars
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Car>>> GetCars()
+        {
+          if (_context.Cars == null)
+          {
+              return NotFound();
+          }
+            return await _context.Cars.ToListAsync();
+        }
+
+        // GET: api/Cars/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Car>> GetCar(int id)
+        {
+          if (_context.Cars == null)
+          {
+              return NotFound();
+          }
+            var car = await _context.Cars.FindAsync(id);
+
+            if (car == null)
+            {
+                return NotFound();
+            }
+
+            return car;
+        }
+
+        // PUT: api/Cars/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCar(int id, Car car)
+        {
+            if (id != car.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(car).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CarExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Cars
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Car>> PostCar(Car car)
+        {
+          if (_context.Cars == null)
+          {
+              return Problem("Entity set 'AppDbContext.Cars'  is null.");
+          }
+            _context.Cars.Add(car);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCar", new { id = car.Id }, car);
+        }
+
+        // DELETE: api/Cars/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCar(int id)
+        {
+            if (_context.Cars == null)
+            {
+                return NotFound();
+            }
+            var car = await _context.Cars.FindAsync(id);
+            if (car == null)
+            {
+                return NotFound();
+            }
+
+            _context.Cars.Remove(car);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CarExists(int id)
+        {
+            return (_context.Cars?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
