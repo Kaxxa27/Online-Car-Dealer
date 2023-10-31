4daeaf1a93e4ef955bd702884f5318711185b8b2
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-10-31 17:06:54 +0300

    Added client WEB.

--- WEB_153503_Kakhnouski/WEB_153503_IdentityServer/Config.cs
+++ WEB_153503_Kakhnouski/WEB_153503_IdentityServer/Config.cs
@@ -1,4 +1,5 @@
-﻿using Duende.IdentityServer.Models;
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
 
 namespace WEB_153503_IdentityServer
 {
@@ -26,33 +27,39 @@ namespace WEB_153503_IdentityServer
         public static IEnumerable<Client> Clients =>
             new Client[]
             {
-            // m2m client credentials flow client
-            new Client
-            {
-                ClientId = "m2m.client",
-                ClientName = "Client Credentials Client",
+                // m2m client credentials flow client
+                new Client
+                {
+                    ClientId = "m2m.client",
+                    ClientName = "Client Credentials Client",
 
-                AllowedGrantTypes = GrantTypes.ClientCredentials,
-                ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = { new Secret("my-secret-key".Sha256()) },
 
-                AllowedScopes = { "scope1" }
-            },
+                    AllowedScopes = { "read", "write", "profile"}
+                },
 
-            // interactive client using code flow + pkce
-            new Client
-            {
-                ClientId = "interactive",
-                ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+                // interactive client using code flow + pkce
+                new Client
+                {
+                    ClientId = "WEB",
+                    ClientSecrets = { new Secret("my-secret-key".Sha256()) },
 
-                AllowedGrantTypes = GrantTypes.Code,
+                    AllowedGrantTypes = GrantTypes.Code,
 
-                RedirectUris = { "https://localhost:44300/signin-oidc" },
-                FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
+                    RedirectUris = { "https://localhost:7001/signin-oidc" },
+                    FrontChannelLogoutUri = "https://localhost:7001/signout-oidc",
+                    PostLogoutRedirectUris = { "https://localhost:7001/signout-callback-oidc" },
 
-                AllowOfflineAccess = true,
-                AllowedScopes = { "openid", "profile", "scope2" }
-            },
+                    AllowOfflineAccess = true,
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "WEB"
+                    }                 
+                },
             };
     }
 }
