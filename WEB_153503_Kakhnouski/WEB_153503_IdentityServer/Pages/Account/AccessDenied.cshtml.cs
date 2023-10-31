d9217ebfa6e79422cd16b4de3012ee02825c50a9
Kaxnovskiy_Eugene <Kaxxa2927@mail.ru>
2023-10-31 15:40:00 +0300

    IdentityServer With ASP.NET added.

---
+++ WEB_153503_Kakhnouski/WEB_153503_IdentityServer/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WEB_153503_IdentityServer.Pages.Account
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
