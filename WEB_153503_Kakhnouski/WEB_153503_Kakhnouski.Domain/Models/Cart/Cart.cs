66f60042dad5445ffd16d6324ef733810cdf988b
Eugene_Kakhnouski <Kaxxa2927@mail.ru>
2023-11-25 16:41:58 +0300

    Added Cart and CartItem models.

---
+++ WEB_153503_Kakhnouski/WEB_153503_Kakhnouski.Domain/Models/Cart/Cart.cs
@@ -0,0 +1,58 @@
+﻿using WEB_153503_Kakhnouski.Domain.Entities;
+
+namespace WEB_153503_Kakhnouski.Domain.Models.Cart;
+
+
+public class Cart
+{
+    /// <summary>
+    /// Список объектов в корзине
+    /// key - идентификатор объекта
+    /// </summary>
+    public Dictionary<int, CartItem> CartItems { get; set; } = new();
+
+    /// <summary>
+    /// Добавить объект в корзину
+    /// </summary>
+    /// <param name="car">Добавляемый объект</param>
+    public virtual void AddToCart(Car car)
+    {
+        if (CartItems.ContainsKey(car.Id))
+            CartItems[car.Id].Quantity++;
+        else
+            CartItems[car.Id] = new CartItem()
+            {
+                Car = car,
+                Quantity = 1
+            };
+    }
+
+    /// <summary>
+    /// Удалить объект из корзины
+    /// </summary>
+    /// <param name="id"> id удаляемого объекта</param>
+    public virtual void RemoveItems(int id)
+    {
+        CartItems.Remove(id);
+    }
+
+    /// <summary>
+    /// Очистить корзину
+    /// </summary>
+    public virtual void ClearAll()
+    {
+        CartItems.Clear();
+    }
+
+    /// <summary>
+    /// Количество объектов в корзине
+    /// </summary>
+    public int Count =>
+        CartItems.Sum(item => item.Value.Quantity);
+
+    /// <summary>
+    /// Общее сумма корзины
+    /// </summary>
+    public double TotalPrice =>
+        CartItems.Sum(item => item.Value.Car.Price * item.Value.Quantity);
+}
---
+++ WEB_153503_Kakhnouski/WEB_153503_Kakhnouski.Domain/Models/Cart/CartItem.cs
@@ -0,0 +1,9 @@
+﻿using WEB_153503_Kakhnouski.Domain.Entities;
+
+namespace WEB_153503_Kakhnouski.Domain.Models.Cart;
+
+public class CartItem
+{
+    public Car Car { get; set; } = null!;
+    public int Quantity { get; set; }
+}
